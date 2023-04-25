1de7828eb2612e7c96d64bf5c2aec5f5ed80c812
Divyata <divyatashankarrathivadekar21@gmail.com>
2023-04-25 19:49:17 +0530

    added a and b

--- Test/src/Test/Sample2.java
+++ Test/src/Test/Sample2.java
@@ -2,7 +2,11 @@ package Test;
 
 public class Sample2 {
 	public static void main(String[] args) {
-		System.out.println("skdfjksjgkh");
+		int a = 5;
+		int b = 6;
+		int c = a+b;
+	System.out.println(c);
+	System.out.println(b-a);
 	}
 
 }
